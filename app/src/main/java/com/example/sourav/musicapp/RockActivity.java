e20392d0aedffec9c7625c727d899ce7a51e7155
Sourav Mondal <souravcristiano502@gmail.com>
2018-09-07 13:59:42 +0530

    adding back button at the action bar

--- app/src/main/java/com/example/sourav/musicapp/RockActivity.java
+++ app/src/main/java/com/example/sourav/musicapp/RockActivity.java
@@ -1,7 +1,11 @@
 package com.example.sourav.musicapp;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,6 +16,10 @@ public class RockActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rock);
+        //setting up button
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         //creating an arrayList of Song object
         ArrayList<Song> songs = new ArrayList<>();
@@ -37,4 +45,15 @@ public class RockActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.rock_list);
         listView.setAdapter(songsAdapter);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
